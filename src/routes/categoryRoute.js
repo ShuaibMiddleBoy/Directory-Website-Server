3265f142a52179f62f2711b0983d572cf055930d
Fazi <faf2001f@gmail.com>
2023-12-06 20:58:56 +0500

    updated directory page

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignIn, isAdmin } = require('../middlewares/authMiddleware');
-const { createCategoryController, updateCategoryController, categoriesController, categoryController, deleteCategoryControllers, deleteCategoryController } = require('../controllers/categoryController');
+const { createCategoryController, updateCategoryController, categoriesController, categoryController, deleteCategoryController, listingsByCategoryController } = require('../controllers/categoryController');
 
 // routes
 
-// create category route
+// Create category route
 router.post('/create-category', createCategoryController);
 
-// update category route
+// Update category route
 router.put('/update-category/:id', updateCategoryController);
 
-// get all categories route
+// Get all categories route
 router.get('/categories', categoriesController);
 
-// get single category route
+// Get single category route
 router.get('/category/:slug', categoryController);
 
-// delete category route
+// Delete category route
 router.delete('/delete-category/:id', deleteCategoryController);
 
+// Fetch listings by category slug route
+router.get('/listings-by-category/:slug', listingsByCategoryController);
 
 module.exports = router;
